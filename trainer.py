73a0499f77fb2230a93f872542878b704c449327
Zhedong Zheng <Zhedong.Zheng@student.uts.edu.au>
2019-08-01 11:25:31 +1000

    Update Multi-GPU

--- trainer.py
+++ trainer.py
@@ -232,8 +232,10 @@ class DGNet_Trainer(nn.Module):
 
         # Random Erasing when training
         if not 'erasing_p' in hyperparameters.keys():
-            hyperparameters['erasing_p'] = 0
-        self.single_re = RandomErasing(probability=hyperparameters['erasing_p'], mean=[0.0, 0.0, 0.0])
+            self.erasing_p = 0
+        else:
+            self.erasing_p = hyperparameters['erasing_p']
+        self.single_re = RandomErasing(probability = self.erasing_p, mean=[0.0, 0.0, 0.0])
 
         if not 'T_w' in hyperparameters.keys():
             hyperparameters['T_w'] = 1
@@ -337,41 +339,40 @@ class DGNet_Trainer(nn.Module):
         cos_dis = 1 - cos(input, target)
         return torch.mean(cos_dis[:])
 
-    def forward(self, x_a, x_b):
-        self.eval()
-        s_a = self.gen_a.encode(self.single(x_a))
-        s_b = self.gen_b.encode(self.single(x_b))
-        f_a, _ = self.id_a(scale2(x_a))
-        f_b, _ = self.id_b(scale2(x_b))
-        x_ba = self.gen_a.decode(s_b, f_a)
-        x_ab = self.gen_b.decode(s_a, f_b)
-        self.train()
-        return x_ab, x_ba
-
-    def gen_update(self, x_a, l_a, xp_a, x_b, l_b, xp_b, hyperparameters, iteration):
-        # ppa, ppb is the same person
-        self.gen_opt.zero_grad()
-        self.id_opt.zero_grad()
-        # encode
+    def forward(self, x_a, x_b, xp_a, xp_b):
         s_a = self.gen_a.encode(self.single(x_a))
         s_b = self.gen_b.encode(self.single(x_b))
         f_a, p_a = self.id_a(scale2(x_a))
         f_b, p_b = self.id_b(scale2(x_b))
-        # autodecode
+        x_ba = self.gen_a.decode(s_b, f_a)
+        x_ab = self.gen_b.decode(s_a, f_b)
         x_a_recon = self.gen_a.decode(s_a, f_a)
         x_b_recon = self.gen_b.decode(s_b, f_b)
- 
-        # encode the same ID different photo
         fp_a, pp_a = self.id_a(scale2(xp_a))
         fp_b, pp_b = self.id_b(scale2(xp_b))
-
         # decode the same person
         x_a_recon_p = self.gen_a.decode(s_a, fp_a)
         x_b_recon_p = self.gen_b.decode(s_b, fp_b)
 
-        # has gradient
-        x_ba = self.gen_a.decode(s_b, f_a)
-        x_ab = self.gen_b.decode(s_a, f_b)
+        # Random Erasing only effect the ID and PID loss.
+        if self.erasing_p > 0:
+            x_a_re = self.to_re(scale2(x_a.clone()))
+            x_b_re = self.to_re(scale2(x_b.clone()))
+            xp_a_re = self.to_re(scale2(xp_a.clone()))
+            xp_b_re = self.to_re(scale2(xp_b.clone()))
+            _, p_a = self.id_a(x_a_re)
+            _, p_b = self.id_b(x_b_re)
+            # encode the same ID different photo
+            _, pp_a = self.id_a(xp_a_re) 
+            _, pp_b = self.id_b(xp_b_re)
+
+        return x_ab, x_ba, s_a, s_b, f_a, f_b, p_a, p_b, pp_a, pp_b, x_a_recon, x_b_recon, x_a_recon_p, x_b_recon_p
+
+    def gen_update(self, x_ab, x_ba, s_a, s_b, f_a, f_b, p_a, p_b, pp_a, pp_b, x_a_recon, x_b_recon, x_a_recon_p, x_b_recon_p, x_a, x_b, xp_a, xp_b, l_a, l_b, hyperparameters, iteration, num_gpu):
+        # ppa, ppb is the same person
+        self.gen_opt.zero_grad()
+        self.id_opt.zero_grad()
+ 
         # no gradient
         x_ba_copy = Variable(x_ba.data, requires_grad=False)
         x_ab_copy = Variable(x_ab.data, requires_grad=False)
@@ -438,29 +439,6 @@ class DGNet_Trainer(nn.Module):
         else:
             self.loss_teacher = 0.0
 
-        # decode again (if needed)
-        if hyperparameters['use_decoder_again']:
-            x_aba = self.gen_a.decode(s_a_recon, f_a_recon) if hyperparameters['recon_x_cyc_w'] > 0 else None
-            x_bab = self.gen_b.decode(s_b_recon, f_b_recon) if hyperparameters['recon_x_cyc_w'] > 0 else None
-        else:
-            self.mlp_w_copy = copy.deepcopy(self.gen_a.mlp_w)
-            self.mlp_b_copy = copy.deepcopy(self.gen_a.mlp_b)
-            self.dec_copy = copy.deepcopy(self.gen_a.dec)  # Error 
-            ID = f_a_recon
-            ID_Style = ID.view(ID.shape[0], ID.shape[1], 1, 1)
-            adain_params_w = self.mlp_w_copy(ID_Style)
-            adain_params_b = self.mlp_b_copy(ID_Style)
-            self.gen_a.assign_adain_params(adain_params_w, adain_params_b, self.dec_copy)
-            x_aba = self.dec_copy(s_a_recon) if hyperparameters['recon_x_cyc_w'] > 0 else None
-
-            ID = f_b_recon
-            ID_Style = ID.view(ID.shape[0], ID.shape[1], 1, 1)
-            adain_params_w = self.mlp_w_copy(ID_Style)
-            adain_params_b = self.mlp_b_copy(ID_Style)
-            self.gen_a.assign_adain_params(adain_params_w, adain_params_b, self.dec_copy)
-            x_bab = self.dec_copy(s_b_recon) if hyperparameters['recon_x_cyc_w'] > 0 else None
-
-
         # auto-encoder image reconstruction
         self.loss_gen_recon_x_a = self.recon_criterion(x_a_recon, x_a)
         self.loss_gen_recon_x_b = self.recon_criterion(x_b_recon, x_b)
@@ -473,17 +451,8 @@ class DGNet_Trainer(nn.Module):
         self.loss_gen_recon_f_a = self.recon_criterion(f_a_recon, f_a) if hyperparameters['recon_f_w'] > 0 else 0
         self.loss_gen_recon_f_b = self.recon_criterion(f_b_recon, f_b) if hyperparameters['recon_f_w'] > 0 else 0
 
-        # Random Erasing only effect the ID and PID loss.
-        if hyperparameters['erasing_p'] > 0:
-            x_a_re = self.to_re(scale2(x_a.clone()))
-            x_b_re = self.to_re(scale2(x_b.clone()))
-            xp_a_re = self.to_re(scale2(xp_a.clone()))
-            xp_b_re = self.to_re(scale2(xp_b.clone()))
-            _, p_a = self.id_a(x_a_re)
-            _, p_b = self.id_b(x_b_re)
-            # encode the same ID different photo
-            _, pp_a = self.id_a(xp_a_re)
-            _, pp_b = self.id_b(xp_b_re)
+        x_aba = self.gen_a.decode(s_a_recon, f_a_recon) if hyperparameters['recon_x_cyc_w'] > 0 else None
+        x_bab = self.gen_b.decode(s_b_recon, f_b_recon) if hyperparameters['recon_x_cyc_w'] > 0 else None
 
         # ID loss AND Tune the Generated image
         if hyperparameters['ID_style']=='PCB':
@@ -505,8 +474,12 @@ class DGNet_Trainer(nn.Module):
         self.loss_gen_cycrecon_x_a = self.recon_criterion(x_aba, x_a) if hyperparameters['recon_x_cyc_w'] > 0 else 0
         self.loss_gen_cycrecon_x_b = self.recon_criterion(x_bab, x_b) if hyperparameters['recon_x_cyc_w'] > 0 else 0
         # GAN loss
-        self.loss_gen_adv_a = self.dis_a.calc_gen_loss(x_ba)
-        self.loss_gen_adv_b = self.dis_b.calc_gen_loss(x_ab)
+        if num_gpu>1:
+            self.loss_gen_adv_a = self.dis_a.module.calc_gen_loss(self.dis_a, x_ba)
+            self.loss_gen_adv_b = self.dis_b.module.calc_gen_loss(self.dis_b, x_ab)
+        else:
+            self.loss_gen_adv_a = self.dis_a.calc_gen_loss(self.dis_a, x_ba)
+            self.loss_gen_adv_b = self.dis_b.calc_gen_loss(self.dis_b, x_ab)
         # domain-invariant perceptual loss
         self.loss_gen_vgg_a = self.compute_vgg_loss(self.vgg, x_ba, x_b) if hyperparameters['vgg_w'] > 0 else 0
         self.loss_gen_vgg_b = self.compute_vgg_loss(self.vgg, x_ab, x_a) if hyperparameters['vgg_w'] > 0 else 0
@@ -604,19 +577,15 @@ hyperparameters['teacher_w'] * self.loss_teacher )  )
 
         return x_a, x_a_recon, x_aba, x_ab1, x_b, x_b_recon, x_bab, x_ba1
 
-    def dis_update(self, x_a, x_b, hyperparameters):
+    def dis_update(self, x_ab, x_ba, x_a, x_b, hyperparameters, num_gpu):
         self.dis_opt.zero_grad()
-        # encode
-        s_a = self.gen_a.encode(self.single(x_a))
-        s_b = self.gen_b.encode(self.single(x_b))
-        f_a, _ = self.id_a(scale2(x_a))
-        f_b, _ = self.id_b(scale2(x_b))
-        # decode (cross domain)
-        x_ba = self.gen_a.decode(s_b, f_a)
-        x_ab = self.gen_b.decode(s_a, f_b)
         # D loss
-        self.loss_dis_a, reg_a = self.dis_a.calc_dis_loss(x_ba.detach(), x_a)
-        self.loss_dis_b, reg_b = self.dis_b.calc_dis_loss(x_ab.detach(), x_b)
+        if num_gpu>1:
+            self.loss_dis_a, reg_a = self.dis_a.module.calc_dis_loss(self.dis_a, x_ba.detach(), x_a)
+            self.loss_dis_b, reg_b = self.dis_b.module.calc_dis_loss(self.dis_b, x_ab.detach(), x_b)
+        else:
+            self.loss_dis_a, reg_a = self.dis_a.calc_dis_loss(self.dis_a, x_ba.detach(), x_a)
+            self.loss_dis_b, reg_b = self.dis_b.calc_dis_loss(self.dis_b, x_ab.detach(), x_b)
         self.loss_dis_total = hyperparameters['gan_w'] * self.loss_dis_a + hyperparameters['gan_w'] * self.loss_dis_b
         print("DLoss: %.4f"%self.loss_dis_total, "Reg: %.4f"%(reg_a+reg_b) )
         if self.fp16:
@@ -665,14 +634,17 @@ hyperparameters['teacher_w'] * self.loss_teacher )  )
         print('Resume from iteration %d' % iterations)
         return iterations
 
-    def save(self, snapshot_dir, iterations):
+    def save(self, snapshot_dir, iterations, num_gpu=1):
         # Save generators, discriminators, and optimizers
         gen_name = os.path.join(snapshot_dir, 'gen_%08d.pt' % (iterations + 1))
         dis_name = os.path.join(snapshot_dir, 'dis_%08d.pt' % (iterations + 1))
         id_name = os.path.join(snapshot_dir, 'id_%08d.pt' % (iterations + 1))
         opt_name = os.path.join(snapshot_dir, 'optimizer.pt')
         torch.save({'a': self.gen_a.state_dict()}, gen_name)
-        torch.save({'a': self.dis_a.state_dict()}, dis_name)
+        if num_gpu>1:
+            torch.save({'a': self.dis_a.module.state_dict()}, dis_name)
+        else:
+            torch.save({'a': self.dis_a.state_dict()}, dis_name)
         torch.save({'a': self.id_a.state_dict()}, id_name)
         torch.save({'gen': self.gen_opt.state_dict(), 'id': self.id_opt.state_dict(),  'dis': self.dis_opt.state_dict()}, opt_name)
 
